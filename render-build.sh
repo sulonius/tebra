d2a09004c8f4b1b44172167a8c1cdcadf1ffcebd
sulonius <103021248+sulonius@users.noreply.github.com>
2024-12-09 20:00:08 +0100

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Pretvaranje linijskih završetaka u LF format (ukoliko je potrebno)
+sed -i 's/\r//' squashfs-root/AppRun
+
+# Dodela izvršnih prava za AppRun fajl
 chmod +x squashfs-root/AppRun
 
